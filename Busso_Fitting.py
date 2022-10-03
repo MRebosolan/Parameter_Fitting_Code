888ccce95b5cb365497eed70a39431dbcb3e5800
MRebosolan <superreba17@gmail.com>
2022-10-03 13:02:15 +0200

    .2 yield found for SACQ

--- Busso_Fitting.py
+++ Busso_Fitting.py
@@ -2,7 +2,3 @@ import numpy as np
 import pandas as pd
 from math import *
 
-gr = np.array([8, 8, 7.5, 6.5, 7, 8, 9, 6.5, 8.5, 7.5, 9.5, 8.5, 8])
-w = np.array([3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 5])
-avg = sum(gr*w)/sum(w)
-print(avg)
---
+++ Functions.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from math import *
+from sklearn.linear_model import LinearRegression
+
+
+def find_slope(x_array, y_array):
+    slopes = (y_array-y_array[0])/(x_array-x_array[0])
+    E_mod = slopes[1]
+    line02 = E_mod*(x_array-0.0002)
+    return slopes, line02
+
+def yield2percent(x_array, y_array):
+    diff = np.abs(y_array-x_array)
+    minimum = np.min(diff)
+    condition = (diff == minimum)
+    location = np.where(condition)[0]
+    return y_array[location]
